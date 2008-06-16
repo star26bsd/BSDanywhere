faac0ccd5a86252fa88c5cf199ceaba1b0f39805
stephan <stephan@0ea659fa-264c-11dd-84bc-a49913644fa9>
2008-06-16 09:06:03 +0000

    Fix .mbin and .msbin population as found by Renè.

--- build.sh
+++ build.sh
@@ -159,8 +159,8 @@ sub_mfsmount() {
         echo "Free memory available, using it for /bin and /sbin."
         mount_mfs -s 24000 swap /.mbin
         mount_mfs -s 48000 swap /.msbin
-        /bin/cp -rp /bin /.mbin
-        /bin/cp -rp /sbin /.msbin
+        /bin/cp -rp /bin/* /.mbin/
+        /bin/cp -rp /sbin/* /.msbin/
         perl -p -i -e 's#^(PATH=)(.*)#\$1/.msbin:/.mbin:\$2#' /root/.profile
         perl -p -i -e 's#^(PATH=)(.*)#\$1/.msbin:/.mbin:\$2#' /home/live/.profile
     fi
