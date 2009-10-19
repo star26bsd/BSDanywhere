40e25d64d073172f03eb09c683ea1bf70eeb3df9
stephan <stephan@0ea659fa-264c-11dd-84bc-a49913644fa9>
2009-10-19 13:26:01 +0000

    fix previous.

--- build.sh
+++ build.sh
@@ -219,7 +219,7 @@ install_template_files() {
 
     # Install window manager specific prerequisites.
     install -d -o 1000 -g 10 -m 755 $IMAGE_ROOT/home/live/.icewm/
-    install -o 1000 -g 10 -m 644 $CWD/home/live/.icewm/shutdown $IMAGE_ROOT/home/live/.icewm/preferences
+    install -o 1000 -g 10 -m 644 $CWD/home/live/.icewm/preferences $IMAGE_ROOT/home/live/.icewm/preferences
     install -o 1000 -g 10 -m 755 $CWD/home/live/.icewm/shutdown $IMAGE_ROOT/home/live/.icewm/shutdown
     install -d -o 1000 -g 10 -m 755 $IMAGE_ROOT/home/live/.config/
     install -d -o 1000 -g 10 -m 755 $IMAGE_ROOT/home/live/.config/menus/
